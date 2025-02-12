210b801d777007a87e7b5f5ea51cf979cf16ee30
Huzaifa Mill <huzefafm53786@gmail.com>
2025-02-12 19:52:01 +0530

    change

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const box2 = document.querySelector(".box2")
 const button2 = document.querySelector(".button2")
-const color = document.querySelector(".box3")
+const body = document.querySelector("body")
 button2.addEventListener("click", () => {
     let code = "#" + Math.floor(Math.random()*16777215).toString(16);
     box2.innerHTML = code;
-    color.style.backgroundColor = code;
+    body.style.backgroundColor = code;
 
 })
